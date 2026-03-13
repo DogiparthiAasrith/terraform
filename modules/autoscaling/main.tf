3517a4fb1041adb5834d6d37650fe0f133dbf950
DogiparthiAasrith <aasrith.dogiparthi@creyentetech.com>
2026-03-13 18:22:21 +0530

    alb

--- modules/autoscaling/main.tf
+++ modules/autoscaling/main.tf
@@ -11,3 +11,23 @@ resource "aws_launch_template" "lt" {
     }
   }
 }
+
+resource "aws_autoscaling_group" "asg" {
+  name                = "${var.project_name}-asg"
+  vpc_zone_identifier = var.private_subnets
+  desired_capacity    = 2
+  max_size            = 3
+  min_size            = 1
+  target_group_arns   = [var.target_group_arn]
+
+  launch_template {
+    id      = aws_launch_template.lt.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "CreatedBy"
+    value               = "Aasrith"
+    propagate_at_launch = true
+  }
+}
