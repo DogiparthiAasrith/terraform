ba0048fb23276f993ba8e46e388c1b9dbc875bdb
DogiparthiAasrith <aasrith.dogiparthi@creyentetech.com>
2026-03-13 17:10:44 +0530

    region

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -4,7 +4,7 @@ terraform {
   backend "s3" {
     bucket  = "terraform-state-bucket-week4"
     key     = "dev/terraform.tfstate"
-    region  = "ap-south-1"
+    region  = "us-east-1"
 
     # NEW: Enables native S3 locking without DynamoDB
     use_lockfile = true
