227169df36dc91aae6de56b116945a1287706431
DogiparthiAasrith <aasrith.dogiparthi@creyentetech.com>
2026-03-16 10:58:32 +0530

    gateway

--- modules/autoscaling/main.tf
+++ modules/autoscaling/main.tf
@@ -1,6 +1,16 @@
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 resource "aws_launch_template" "lt" {
   name_prefix   = "${var.project_name}-lt"
-  image_id      = "ami-0f58b397bc5c1f2e8"
+  image_id      = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
 
   tag_specifications {
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -54,3 +54,36 @@ resource "aws_subnet" "private2" {
 resource "aws_default_security_group" "default" {
   vpc_id = aws_vpc.main.id
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name      = "${var.project_name}-igw"
+    CreatedBy = "Aasrith"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name      = "${var.project_name}-public-rt"
+    CreatedBy = "Aasrith"
+  }
+}
+
+resource "aws_route_table_association" "public1" {
+  subnet_id      = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
